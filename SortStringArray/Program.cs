9560aa403f3509fa6ade04d5da8b147e9cacfbb8
Orkhansha <orkhansha@code.edu.az>
2022-06-22 02:46:53 +0400

    Added ReverseStringProgram

---
+++ SortStringArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SortStringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = "Orxan";
+            string reversedName = backswardsStringConvert(name);
+            Console.WriteLine(name + " written backwards is "+reversedName);
+            Console.ReadLine();
+        }
+
+        private static string backswardsStringConvert(string name)
+        {
+            string r = "";
+
+            for (int i = name.Length-1; i >=0; i--)
+            {
+                r += name[i];
+            }
+            return r;
+        }
+    }
+}
